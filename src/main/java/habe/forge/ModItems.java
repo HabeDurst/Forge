9414dd353739f0fa3c4459dd9c38464a811c3e81
HabeDurst <habedurst@proton.me>
2026-04-10 11:35:39 +0200

    ä

--- src/main/java/habe/forge/ModItems.java
+++ src/main/java/habe/forge/ModItems.java
@@ -41,6 +41,8 @@ public class ModItems {
         tag.putInt("CustomModelData", 101);
         stack.set(DataComponents.CUSTOM_DATA, CustomData.of(tag));
 
+        stack.set(DataComponents.ITEM_MODEL, Identifier.fromNamespaceAndPath("forge", modelPath));
+
         stack.set(DataComponents.ATTRIBUTE_MODIFIERS,
                 ItemAttributeModifiers.builder()
                         .add(attribute,
